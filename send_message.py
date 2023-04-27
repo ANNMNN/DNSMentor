452c0f79e0b7db9e898afc4c2ecf676d94ac4ae8
ANNMNN <100520461+ANNMNN@users.noreply.github.com>
2023-04-28 01:30:43 +1000

    Delete send_message.py

--- send_message.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def telegram_bot_sendtext(bot_message, bot_chatID='1674351438'):
-    bot_token = '5712199811:AAF7-n5ceuzwIRcBRr7yaNZnvpk-oqAEqYk'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-    response = requests.get(send_text)
-    return response.json()
-
